1a0c3f19c4d55bf23625b67b976552b3c6720b82
Khanh Tieu <tieutruongkhanh@gmail.com>
2025-10-01 16:43:39 +0700

    change home icon

--- src/components/Layout/components/LeftSide/LeftSide.jsx
+++ src/components/Layout/components/LeftSide/LeftSide.jsx
@@ -146,18 +146,12 @@ export default function LeftSide() {
           <OtherFunction
             image={
               <svg
+                fill="currentColor"
+                viewBox="0 0 48 48"
                 xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
                 className="size-7"
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
-                />
+                <path d="M24.95 7.84a1.5 1.5 0 0 0-1.9 0l-16.1 13.2a1.5 1.5 0 0 0 .95 2.66h2.33l1.2 13.03A2.5 2.5 0 0 0 13.9 39h7.59a1 1 0 0 0 1-1v-9.68a1 1 0 0 1 1-1h1a1 1 0 0 1 1 1V38a1 1 0 0 0 1 1h7.59a2.5 2.5 0 0 0 2.49-2.27l1.19-13.03h2.33a1.5 1.5 0 0 0 .95-2.66l-16.1-13.2Z"></path>
               </svg>
             }
             title="Đề xuất"
